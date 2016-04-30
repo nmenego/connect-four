2e4cae6c5e5b35a8e113785f0d4c477e7cd0b599
nmenego <nmenego@gmail.com>
2016-04-30 20:51:09 +0800

    code cleanup, add css, error checking

--- client/main.js
+++ client/main.js
@@ -1,17 +1,19 @@
 /**
+ * Just a simple MeteorJS app.
  * @author NMEnego 2016
  */
 import {
     Template
-} from 'meteor/templating';
+} from "meteor/templating";
 import {
     ReactiveVar
-} from 'meteor/reactive-var';
+} from "meteor/reactive-var";
 
-import './main.html';
+import "./main.html";
 
 // container for state table
 var STATE_TABLE = null;
+var VS_HUMAN = true;
 const RED = "red";
 const BLACK = "black";
 const WHITE = "white";
@@ -19,6 +21,8 @@ const ONGOING = "Ongoing...";
 const MAX_X = 7;
 const MAX_Y = 6;
 const IN_A_ROW = 4;
+const WON = "won";
+const DRAW = "draw";
 
 // meteorjs helpers
 Template.hello.onCreated(function helloOnCreated() {
@@ -39,41 +43,62 @@ Template.hello.helpers({
 
 // meteorjs helpers
 Template.hello.events({
-    'click button' (event, instance) {
+    "click button" (event, instance) {
         Template.instance().player.set(RED);
         Template.instance().won.set(ONGOING);
         drawBoard();
+        if (!VS_HUMAN) {
+            // randomize first player.
+            var headsOrTails = Math.floor(Math.random() * 2);
+            if (headsOrTails) {
+                computerMove();
+            }
+        }
+    },
+    "click #a" (event) {
+        VS_HUMAN = true;
+        document.getElementById("computer_level").disabled = true;
+    },
+    "click #b" (event) {
+        VS_HUMAN = false;
+        document.getElementById("computer_level").disabled = false;
     },
     // when the canvas is clicked...
-    'click #mycanvas' (event) {
+    "click #mycanvas" (event) {
         if (Template.instance().won.get() !== ONGOING) {
             alert("Game over. Please restart the game.");
+            document.getElementById("reset").focus();
             return;
         }
-        // calculate click.
-        var x = event.pageX,
-            y = event.pageY;
+        // calculate click with respect to canvas
         var canvas = event.target;
+        var rect = canvas.getBoundingClientRect();
+        var x = event.clientX - rect.left;
+        var y = event.clientY - rect.top;
+
+        // draw move.
         if (canvas.getContext) {
             // increment the counter when button is clicked
             var currentPlayer = Template.instance().player;
-            var dropped = drop(canvas.getContext('2d'), Math.floor(x / 100), currentPlayer.get());
+            var dropped = drop(canvas.getContext("2d"), Math.floor(x / 100), currentPlayer.get());
             if (dropped) {
 
                 // check if game is won.
-                if (checkWin(currentPlayer.get())) {
+                if (checkWin(currentPlayer.get(), STATE_TABLE) === WON) {
                     Template.instance().won.set(currentPlayer.get() + " wins!");
                     alert(currentPlayer.get() + " wins!");
+                    document.getElementById("reset").focus();
                 }
 
                 // change players
                 getNextPlayer(currentPlayer);
+                //if()
             }
         }
     }
 });
 
- // draw the board
+// draw the board
 function drawBoard() {
 
     // initialize array
@@ -85,7 +110,7 @@ function drawBoard() {
     // canvas
     var canvas = document.getElementById("mycanvas");
     if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
+        var ctx = canvas.getContext("2d");
         // clear previous data
         ctx.clearRect(0, 0, ctx.width, ctx.height);
         // main rectangle.
@@ -119,8 +144,8 @@ function drawCircle(ctx, x, y, color) {
 // drop a coin.
 function drop(ctx, x, color) {
 
-    // buffers automatically when created
-    var snd = new Audio("coin-drop-5.mp3");
+    // might not work for older browsers.
+    var snd = new Audio("coin-drop-5.mp3"); // buffers automatically when created
     console.log("dropping " + color + "@" + x);
     for (y = MAX_Y - 1; y >= 0; y--) {
         if (STATE_TABLE[x][y] === WHITE) {
@@ -143,13 +168,13 @@ function getNextPlayer(player) {
 }
 
 // checking of winners
-function checkWin(currentPlayer) {
+function checkWin(currentPlayer, table) {
     // check horizontals (starting from the bottom)
     for (y = MAX_Y - 1, count = 0; y >= 0; y--, count = 0) {
         for (x = 0; x < MAX_X; x++) {
-            if (STATE_TABLE[x][y] === currentPlayer) {
+            if (table[x][y] === currentPlayer) {
                 count++;
-                if (count >= IN_A_ROW) return true;
+                if (count >= IN_A_ROW) return WON;
             } else {
                 count = 0;
             }
@@ -158,9 +183,9 @@ function checkWin(currentPlayer) {
     // check verticals (starting from the left)
     for (x = 0, count = 0; x < MAX_X; x++, count = 0) {
         for (y = MAX_Y - 1; y >= 0; y--) {
-            if (STATE_TABLE[x][y] === currentPlayer) {
+            if (table[x][y] === currentPlayer) {
                 count++;
-                if (count >= IN_A_ROW) return true;
+                if (count >= IN_A_ROW) return WON;
             } else {
                 count = 0;
             }
@@ -169,9 +194,9 @@ function checkWin(currentPlayer) {
     // check diagonals (top-to-right)
     for (x = 0, count = 0; x <= MAX_X - IN_A_ROW; x++, count = 0) {
         for (y = 0, xTemp = x; y < MAX_Y && xTemp < MAX_X; y++, xTemp++) {
-            if (STATE_TABLE[xTemp][y] === currentPlayer) {
+            if (table[xTemp][y] === currentPlayer) {
                 count++;
-                if (count >= IN_A_ROW) return true;
+                if (count >= IN_A_ROW) return WON;
             } else {
                 count = 0;
             }
@@ -180,9 +205,9 @@ function checkWin(currentPlayer) {
     // check diagonals (left-to-bottom)
     for (y = 1, count = 0; y <= MAX_Y - IN_A_ROW; y++, count = 0) {
         for (x = 0, yTemp = y; x < MAX_X && yTemp < MAX_Y; x++, yTemp++) {
-            if (STATE_TABLE[x][yTemp] === currentPlayer) {
+            if (table[x][yTemp] === currentPlayer) {
                 count++;
-                if (count >= IN_A_ROW) return true;
+                if (count >= IN_A_ROW) return WON;
             } else {
                 count = 0;
             }
@@ -191,9 +216,9 @@ function checkWin(currentPlayer) {
     // check diagonals (top-to-left)
     for (x = MAX_X - 1, count = 0; x >= IN_A_ROW - 1; x--, count = 0) {
         for (y = 0, xTemp = x; y < MAX_Y && xTemp >= 0; y++, xTemp--) {
-            if (STATE_TABLE[xTemp][y] === currentPlayer) {
+            if (table[xTemp][y] === currentPlayer) {
                 count++;
-                if (count >= IN_A_ROW) return true;
+                if (count >= IN_A_ROW) return WON;
             } else {
                 count = 0;
             }
@@ -202,13 +227,21 @@ function checkWin(currentPlayer) {
     // check diagonals (right-to-bottom)
     for (y = 1, count = 0; y <= MAX_Y - IN_A_ROW; y++, count = 0) {
         for (x = MAX_X - 1, yTemp = y; yTemp < MAX_Y && x >= 0; x--, yTemp++) {
-            if (STATE_TABLE[x][yTemp] === currentPlayer) {
+            if (table[x][yTemp] === currentPlayer) {
                 count++;
-                if (count >= IN_A_ROW) return true;
+                if (count >= IN_A_ROW) return WON;
             } else {
                 count = 0;
             }
         }
     }
-    return false;
+    // check draw (all states black/red)
+    for (x = 0; x < MAX_X; x++) {
+        // we just have to check top row y=0
+        if (table[x][0] === "white") {
+            return ONGOING;
+        }
+    }
+
+    return DRAW;
 }
